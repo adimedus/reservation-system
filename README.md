05234629268cddfa8adf5919f8308915d5704578
adimedus <adimedus@gmail.com>
2024-11-29 01:11:16 +0100

    První commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Co má program dělat?
+Jednoduše vidět stoly po zavolání, jaká data jsou volná, jak jsou velké
+musí umožnit spojit stoly
+následně napojit na TG
+Jednoduché UI pro bookování je cíl, tj ideálně klikačka
+běžně by měl ukázat 7 dní dopředu
+dále by mohl sbírat data, jak moc byl podnik využit
+
+Momentálně používají tg skupinu na rezervace
+Mělo by možné být volat i textově, co chci
+
+Mimo to bude dělat i webové UI pro obě možnosti
+
+Co to nemá dělat - obtižnější práci - tj není nutné psát přesný počet lidí
+Může to dělat grafy jak které hodiny je podnik obsazen
+
+Stoly můžou mít označení jménem
+
+Tj momentální stoly - neon, u záchodu, 4ka obraz, 4k okno, dvojka okno, dvojka vchod, velky stul
+Muze to vygenerovat mapku stolu - zelene a cervene dle obsazenosti
+
+Prvni fáze
+Udelat tui - kde napisu rezervace pocet lidi a navrhne mi to mozne volby
+Nasledne rozchodit hodiny
+
+Melo by to mit osetrenou otviraci dobu
---
+++ main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+type table struct {
+	name      string
+	capacity  int
+	free      bool
+	fulluntil time.Time
+}
+
+func filter(data []table, f func(table) bool) []table {
+
+	fltd := make([]table, 0)
+	free := true
+
+	for _, table := range data {
+		if table.free == free {
+			if f(table) {
+				fltd = append(fltd, table)
+			}
+		}
+	}
+
+	return fltd
+}
+
+func main() {
+
+	now := time.Now()
+	czechMonths := map[string]string{
+		"January":   "ledna",
+		"February":  "února",
+		"March":     "března",
+		"April":     "dubna",
+		"May":       "května",
+		"June":      "června",
+		"July":      "července",
+		"August":    "srpna",
+		"September": "září",
+		"October":   "října",
+		"November":  "listopadu",
+		"December":  "prosince",
+	}
+	longFormat := now.Format("2. January 2006 15:04:05")
+	for eng, czech := range czechMonths {
+		longFormat = strings.ReplaceAll(longFormat, eng, czech)
+	}
+	fmt.Println("Datum a čas (česky):", longFormat)
+
+	tables := []table{
+		{"Velký", 6, true, time.Time{}},
+		{"Dveře", 2, true, time.Time{}},
+		{"Okno", 2, true, time.Time{}},
+		{"Čtyřka okno", 4, true, time.Time{}},
+		{"Obraz", 4, true, time.Time{}},
+		{"Neon", 2, false, time.Now().Add(2 * time.Hour)},
+		{"Záchod", 2, true, time.Time{}},
+	}
+
+	fmt.Println("List of tables:")
+
+	freefortwo := 2
+	freeforfour := 4
+	freeforsix := 6
+
+	freecap2 := filter(tables, func(i table) bool {
+		return i.capacity == freefortwo
+	})
+	freecap4 := filter(tables, func(i table) bool {
+		return i.capacity == freeforfour
+	})
+	freecap6 := filter(tables, func(i table) bool {
+		return i.capacity == freeforsix
+	})
+
+	println("Otevřeno je od 13:00 do 0:00 pondělí až čtvrtek, pátek a sobota od 13:00 do 1:00, neděle pak 15:00 až 23:00")
+
+	println("Pro kolik lidí chceš rezervovat stůl?")
+
+	var answer int
+	var whattable string = "Velký"
+	fmt.Scanf("%d\n", &answer)
+
+	if answer <= 2 && len(freecap2) == 0 {
+		fmt.Println("Pro dva stůl není, co takhle větší stůl: ")
+		for _, t := range freecap4 {
+			fmt.Printf("Jméno: %s, Kapacita: %d lidi\n", t.name, t.capacity)
+		}
+		for _, t := range freecap6 {
+			fmt.Printf("Jméno: %s, Kapacita: %d lidí\n", t.name, t.capacity)
+		}
+	} else if answer <= 2 {
+		fmt.Println("Volné stoly jsou:")
+		for _, t := range freecap2 {
+			fmt.Printf("Jméno: %s, Kapacita: %d lidi\n", t.name, t.capacity)
+		}
+	} else if answer <= 4 && len(freecap4) == 0 {
+		fmt.Println("Nemáme stůl pro čtyři volný, máme ale volný:")
+		for _, t := range freecap6 {
+			fmt.Printf("Jméno: %s, Kapacita: %d lidí\n", t.name, t.capacity)
+		}
+	} else if answer <= 4 {
+		fmt.Println("Volné stoly jsou:")
+		for _, t := range freecap4 {
+			fmt.Printf("Jméno: %s, Kapacita: %d lidi\n", t.name, t.capacity)
+		}
+	} else if answer <= 6 && len(freecap6) == 0 {
+		fmt.Printf("Nemáme volný stůl pro tolik lidí, chceš stoly spojit?(Ano/Ne)")
+	} else if answer <= 6 {
+		fmt.Println("Volný je:")
+		for _, t := range freecap6 {
+			fmt.Printf("Jméno: %s, Kapacita: %d lidí\n", t.name, t.capacity)
+		}
+	} else {
+		fmt.Println("Nemáme tak velký stůl, chceš spojit stoly?(Ano/Ne)")
+
+	}
+
+	fmt.Println("Zadej jméno stolu, co chceš rezervovat: ")
+	fmt.Scanf("%s", &whattable)
+	for t := range tables {
+		if tables[t].name == whattable {
+			tables[t].free = false
+			tables[t].fulluntil = time.Now().Add(3 * time.Hour)
+			fmt.Printf("Zarezervovaný stůl do: Jméno: %s, Kapacita: %d, Volný: %s\n", tables[t].name, tables[t].capacity, formatCzechDate(tables[t].fulluntil, czechMonths))
+			break
+		}
+	}
+	for _, t := range tables {
+		if !t.free {
+			if time.Now().After(t.fulluntil) {
+				fmt.Printf("Stůl %s už není rezervovaný.\n", t.name)
+				t.free = true
+			} else {
+				fmt.Printf("Stůl %s je rezervovaný %s\n", t.name, formatCzechDate(t.fulluntil, czechMonths))
+			}
+		}
+	}
+
+}
+
+func formatCzechDate(t time.Time, czechMonths map[string]string) string {
+	englishFormat := t.Format("2. January 2006 15:04:05")
+	for eng, czech := range czechMonths {
+		englishFormat = strings.ReplaceAll(englishFormat, eng, czech)
+	}
+	return englishFormat
+}
